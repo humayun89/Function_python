8e7c312e105e840e1dff629c3c9c956f382a09da
HumayunRChy <humayun.chowdhury@metropolia.fi>
2022-11-11 03:55:59 +0200

    Space correction

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 input_1 = 10  ## Don't modify this
 input_2 = 40  ## Don't modify this.
 
-
 ## TODO:
 ## Implement a function called sum_fun  that takes two inputs and returns their sum.
 def sum_fun(input_1, input_2):
