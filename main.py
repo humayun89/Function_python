157a3bd3914aeb35d16a6a4c73914e65833cea39
HumayunRChy <humayun.chowdhury@metropolia.fi>
2022-11-11 03:55:20 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# function define:
+input_1 = 10  ## Don't modify this
+input_2 = 40  ## Don't modify this.
+
+
+## TODO:
+## Implement a function called sum_fun  that takes two inputs and returns their sum.
+def sum_fun(input_1, input_2):
+    return input_1 + input_2
+
+result_sum = sum_fun(input_1, input_2)
+print(result_sum)
