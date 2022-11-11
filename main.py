0d66b449522d75c7a2851e0080537f80e576aa0b
HumayunRChy <humayun.chowdhury@metropolia.fi>
2022-11-11 03:59:07 +0200

    Space correction

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ input_2 = 40  ## Don't modify this.
 ## Implement a function called sum_fun  that takes two inputs and returns their sum.
 def sum_fun(input_1, input_2):
     return input_1 + input_2
-
+# multiplication using define function:
 result_sum = sum_fun(input_1, input_2)
 print(result_sum)
+
+x=500
+y= 200
+def multiply(x,y):
+    return x*y
+result=multiply(x,y)
+print(result)
